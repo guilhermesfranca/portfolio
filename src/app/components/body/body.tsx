2fd593b193a700e0bcdf70e199444c8c7d0e2e26
guilhermesfranca <guilhermesfranca@outlook.com>
2025-01-26 02:27:43 -0300

    atualizei a home do portfolio com a imagem e adicionei os links para as midias sociais

--- src/app/components/body/body.tsx
+++ src/app/components/body/body.tsx
@@ -1,62 +1,15 @@
 "use client";
 import styles from "./body.module.css";
 import Image from "next/image";
-import { FaGithub } from "react-icons/fa";
-import { FaWhatsapp } from "react-icons/fa";
-import { SiGmail } from "react-icons/si";
-import { FaLinkedinIn } from "react-icons/fa6";
+import home from '/public/home.png'
+import Body_text from "../body_text/body_text";
 
 export default function Body() {
   return (
-    <main className={styles.div_apresentacao}>
-      <div className={styles.text_apresentacao}>
-        <h3>Olá, meu nome é</h3>
-        <strong>Guiherme França</strong>
-        <p>
-          Desenvolvedor <span>Front-end</span>
-        </p>
-      </div>
-      <p>
-        Desenvolvendo soluções web que conectam ideias e pessoas com{" "}
-        <span>código</span> e <span>criatividade</span>
-      </p>
-      <div className={styles.social}>
-        <button
-          onClick={() =>
-            window.open("https://github.com/guilhermesfranca", "_blank")
-          }
-        >
-          <FaGithub />
-        </button>
-        <button
-          onClick={() =>
-            window.open(
-              "https://www.linkedin.com/in/guilhermesfranca/",
-              "_blank"
-            )
-          }
-        >
-          <FaLinkedinIn />
-        </button>
-        <button
-          onClick={() =>
-            (window.location.href = "mailto:guilhermesfranca@outlook.com")
-          }
-        >
-          <SiGmail />
-        </button>{" "}
-        <button
-          onClick={() =>
-            window.open(
-              "https://wa.me/5531996961455?text=Ol%C3%A1%2C%20vi%20seu%20Porfolio%20e%20gostei",
-              "_blank"
-            )
-          }
-        >
-          <FaWhatsapp />
-        </button>
-      </div>
-      <button className={styles.cv}>Download CV</button>
+    <main className={styles.main}>
+      <Body_text />
+      <Image className={styles.home_img} src={home} alt={'home'}/>
+      
     </main>
   );
 }
---
+++ src/app/components/body_text/body_text.tsx
@@ -0,0 +1,62 @@
+"use client";
+import styles from "./body_text.module.css";
+import Image from "next/image";
+import { FaGithub } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { FaLinkedinIn } from "react-icons/fa6";
+
+export default function Body_text() {
+  return (
+    <main className={styles.div_apresentacao}>
+      <div className={styles.text_apresentacao}>
+        <h3>Olá, meu nome é</h3>
+        <strong>Guiherme França</strong>
+        <p>
+          Desenvolvedor <span>Front-end</span>
+        </p>
+      </div>
+      <p>
+        Desenvolvendo soluções web que conectam ideias e pessoas com{" "}
+        <span>código</span> e <span>criatividade</span>
+      </p>
+      <div className={styles.social}>
+        <button
+          onClick={() =>
+            window.open("https://github.com/guilhermesfranca", "_blank")
+          }
+        >
+          <FaGithub />
+        </button>
+        <button
+          onClick={() =>
+            window.open(
+              "https://www.linkedin.com/in/guilhermesfranca/",
+              "_blank"
+            )
+          }
+        >
+          <FaLinkedinIn />
+        </button>
+        <button
+          onClick={() =>
+            (window.location.href = "mailto:guilhermesfranca@outlook.com")
+          }
+        >
+          <SiGmail />
+        </button>{" "}
+        <button
+          onClick={() =>
+            window.open(
+              "https://wa.me/5531996961455?text=Ol%C3%A1%2C%20vi%20seu%20Porfolio%20e%20gostei",
+              "_blank"
+            )
+          }
+        >
+          <FaWhatsapp />
+        </button>
+      </div>
+      <button className={styles.cv}>Download CV</button>
+    </main>
+  );
+}
