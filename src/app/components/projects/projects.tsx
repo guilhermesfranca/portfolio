e31ac7cab63e318a7f0df0920c883af9d9458f5c
guilhermesfranca <guilhermesfranca@outlook.com>
2025-01-26 23:11:32 -0300

    export commit

--- src/app/components/projects/projects.tsx
+++ src/app/components/projects/projects.tsx
@@ -5,67 +5,64 @@ import Projects_comp from "./projects_comp/projects_comp";
 import barber from "/public/barber.gif";
 
 export default function Projects() {
+  const projetos = [
+    {
+      name: "Barber França",
+      descricao: "Descrição do projeto aqui.",
+      image: barber,
+      repositorio: "https://github.com/guilhermesfranca/barberFranca",
+      site: "https://www.google.com.br/?hl=pt-BR",
+    },
+    {
+      name: "Barber França",
+      descricao: "Descrição do projeto aqui.",
+      image: barber,
+      repositorio: "https://github.com/guilhermesfranca/barberFranca",
+      site: "https://www.google.com.br/?hl=pt-BR",
+    },
+    {
+      name: "Barber França",
+      descricao: "Descrição do projeto aqui.",
+      image: barber,
+      repositorio: "https://github.com/guilhermesfranca/barberFranca",
+      site: "https://www.google.com.br/?hl=pt-BR",
+    },
+    {
+      name: "Barber França",
+      descricao: "Descrição do projeto aqui.",
+      image: barber,
+      repositorio: "https://github.com/guilhermesfranca/barberFranca",
+      site: "https://www.google.com.br/?hl=pt-BR",
+    },
+    {
+      name: "Barber França",
+      descricao: "Descrição do projeto aqui.",
+      image: barber,
+      repositorio: "https://github.com/guilhermesfranca/barberFranca",
+      site: "https://www.google.com.br/?hl=pt-BR",
+    },
+    {
+      name: "Barber França",
+      descricao: "Descrição do projeto aqui.",
+      image: barber,
+      repositorio: "https://github.com/guilhermesfranca/barberFranca",
+      site: "https://www.google.com.br/?hl=pt-BR",
+    },
+    
+  ];
+  
   return (
     <div className={styles.projects}>
-      <Projects_comp
-        name={"Barber França"}
-        descricao={`Site de apresentação de uma barbearia que oferece serviços variados para
-                diferentes tipos de clientes. Desenvolvidao em ReactJS com HTML, CSS e JavaScript,
-                possui uma interface responsiva e recursos de troca de tema de fundo.`}
-        image={barber}
-        repositorio="https://github.com/guilhermesfranca/barberFranca"
-        site="https://www.google.com.br/?hl=pt-BR"
-      />
-
-      <Projects_comp
-        name={"Barber França"}
-        descricao={`Site de apresentação de uma barbearia que oferece serviços variados para
-                diferentes tipos de clientes. Desenvolvidao em ReactJS com HTML, CSS e JavaScript,
-                possui uma interface responsiva e recursos de troca de tema de fundo.`}
-        image={barber}
-        repositorio="https://github.com/guilhermesfranca/barberFranca"
-        site="https://www.google.com.br/?hl=pt-BR"
-      />
-
-      <Projects_comp
-        name={"Barber França"}
-        descricao={`Site de apresentação de uma barbearia que oferece serviços variados para
-                diferentes tipos de clientes. Desenvolvidao em ReactJS com HTML, CSS e JavaScript,
-                possui uma interface responsiva e recursos de troca de tema de fundo.`}
-        image={barber}
-        repositorio="https://github.com/guilhermesfranca/barberFranca"
-        site="https://www.google.com.br/?hl=pt-BR"
-      />
-
-      <Projects_comp
-        name={"Barber França"}
-        descricao={`Site de apresentação de uma barbearia que oferece serviços variados para
-                diferentes tipos de clientes. Desenvolvidao em ReactJS com HTML, CSS e JavaScript,
-                possui uma interface responsiva e recursos de troca de tema de fundo.`}
-        image={barber}
-        repositorio="https://github.com/guilhermesfranca/barberFranca"
-        site="https://www.google.com.br/?hl=pt-BR"
-      />
-
-      <Projects_comp
-        name={"Barber França"}
-        descricao={`Site de apresentação de uma barbearia que oferece serviços variados para
-                diferentes tipos de clientes. Desenvolvidao em ReactJS com HTML, CSS e JavaScript,
-                possui uma interface responsiva e recursos de troca de tema de fundo.`}
-        image={barber}
-        repositorio="https://github.com/guilhermesfranca/barberFranca"
-        site="https://www.google.com.br/?hl=pt-BR"
-      />
-
-      <Projects_comp
-        name={"Barber França"}
-        descricao={`Site de apresentação de uma barbearia que oferece serviços variados para
-                diferentes tipos de clientes. Desenvolvidao em ReactJS com HTML, CSS e JavaScript,
-                possui uma interface responsiva e recursos de troca de tema de fundo.`}
-        image={barber}
-        repositorio="https://github.com/guilhermesfranca/barberFranca"
-        site="https://www.google.com.br/?hl=pt-BR"
-      />
+      {projetos.map((projeto, index) => (
+        <Projects_comp
+          key={index}
+          name={projeto.name}
+          descricao={projeto.descricao}
+          image={projeto.image}
+          repositorio={projeto.repositorio}
+          site={projeto.site}
+        />
+      ))}
     </div>
   );
 }
